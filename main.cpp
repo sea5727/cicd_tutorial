929057de5cf77c14ce5e7b6d835da27bfaf30801
sea5727 <sea5727@naver.com>
2020-05-28 23:43:24 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "add1.hpp"
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    std::cout << "Hello, world!!!!\n";
 
     int result = proc_plus(1, 10);
     printf("result : %d\n", result);
